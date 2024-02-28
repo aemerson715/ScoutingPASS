b43cb75d6347880e1cac9a85591fb55dc645e6fd
aemerson715 <156485129+aemerson715@users.noreply.github.com>
2024-02-27 18:46:19 -0600

    Update crescendo_config.js
    Added TBA Key

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024txfor", 
+      "defaultValue": "2023txfor", 
       "required": "true"
     },
     { "name": "Match Level",
