7ca1eb75a5a5d8cd993c9c29bd99dfd8cf12ca3b
aemerson715 <156485129+aemerson715@users.noreply.github.com>
2024-02-29 17:14:49 -0600

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -82,7 +82,7 @@ var config_data = `
       "type": "clickable_image",
       "filename": "2024/field_image.png",
       "clickRestriction": "eight",
-      "shape": "circle 5 black red true"
+      "shape": "circle 5 black red true",
     }
   ],
   "teleop": [
