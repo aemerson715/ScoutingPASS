4c30d403476b0316fce6ba34f5b820d8231ba9cb
aemerson715 <156485129+aemerson715@users.noreply.github.com>
2025-01-25 14:27:37 -0600

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -5,7 +5,7 @@ var config_data = `
   "page_title": "REEFSCAPE",
   "checkboxAs": "10",
   "prematch": [
-    { "name": "Scouter Initials - TEST3",
+    { "name": "Scouter Initials - TEST4",
       "code": "s",
       "type": "scouter",
       "size": 5,
@@ -70,6 +70,16 @@ var config_data = `
       "code": "al",
       "type": "bool"
     },
+    { "name": "Reef Algae",
+      "code": "ara",
+      "type":"radio",
+      "choices": {
+        "ir": "In Robot<br>",
+        "of": "On Floor<br>",
+        "x": "Not attempted"
+      },
+      "defaultValue": "x"
+    }
     { "name": "Coral L3",
       "code": "ac3",
       "type": "counter"
