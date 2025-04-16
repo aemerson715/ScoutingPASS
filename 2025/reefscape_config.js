bb05dec0e6ea79bd7c2932d95b37db003b55ea47
aemerson715 <156485129+aemerson715@users.noreply.github.com>
2025-04-16 13:26:49 -0500

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025TXCMP1",
+      "defaultValue": "2025NEWTON",
       "required": "true"
     },
     { "name": "Match Level",
